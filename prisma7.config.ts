37d17ccb9e51ec72cc3728a8154af358a8b79dbe
AnshuKumar16 <jiyasony321@gmail.com>
2026-09-25 01:28:55 +0530

    Prepare My Store for production deployment

--- prisma7.config.ts
+++ prisma7.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
 
   datasource: {
-    url: process.env["DATABASE_URL"],
+  url: process.env["DIRECT_URL"],
     shadowDatabaseUrl: process.env["SHADOW_DATABASE_URL"],
   },
 });
